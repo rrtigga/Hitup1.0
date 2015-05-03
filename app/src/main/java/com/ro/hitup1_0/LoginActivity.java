d569bfdaaaf0d91cd835184257ce02a06e53dc0d
rrtigga <rrtigga@ucdavis.edu>
2015-05-03 00:36:38 -0700

    Buttons: Login -> Main. Adding Logos

--- app/src/main/java/com/ro/hitup1_0/LoginActivity.java
+++ app/src/main/java/com/ro/hitup1_0/LoginActivity.java
@@ -1,20 +1,33 @@
 package com.ro.hitup1_0;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
 
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity {
 
     //Give your SharedPreferences file a name and save it to a static variable
     public static final String PREFS_NAME = "MyPrefsFile";
-
+    LoginButton loginButton;
+    Button email_button;
+    Button google_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         SharedPreferences settings = getSharedPreferences(LoginActivity.PREFS_NAME, 0);
 //Get "hasLoggedIn" value. If the value doesn't exist yet false is returned
@@ -31,8 +44,32 @@ public class LoginActivity extends Activity {
             startActivity(intent);
             LoginActivity.this.finish();
         }
-        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
         setContentView(R.layout.activity_login);
+
+        google_button=(Button)findViewById(R.id.google_button);
+        google_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        email_button= (Button)findViewById(R.id.email_button);
+        email_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
     }
 
     @Override
@@ -56,4 +93,8 @@ public class LoginActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
