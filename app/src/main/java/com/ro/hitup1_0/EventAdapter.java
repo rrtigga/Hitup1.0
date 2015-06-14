19e37c5235b07a17bd0d3f34da3b02bd03862c8d
rrtigga <rrtigga@ucdavis.edu>
2015-06-13 20:51:04 -0700

    Still need to work on refreshEvents(), but added SMS Intent for hardcoded phone number. Made the context static, not sure if that's okay.

--- app/src/main/java/com/ro/hitup1_0/EventAdapter.java
+++ app/src/main/java/com/ro/hitup1_0/EventAdapter.java
@@ -2,10 +2,12 @@ package com.ro.hitup1_0;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,7 +20,8 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ContactViewH
 
     private  Activity activity;
     private List<Event> contactList;
-    private Context context;
+    //idk if this should be static
+    private static Context context;
 
     String profile_pic_url;
 
@@ -30,7 +33,6 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ContactViewH
 
     }
 
-
     @Override
     public int getItemCount() {
         return contactList.size();
@@ -41,27 +43,17 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ContactViewH
     public void onBindViewHolder(ContactViewHolder contactViewHolder, int i) {
         Event ci = contactList.get(i);
         contactViewHolder.vName.setText(ci.name);
-        //Picasso.with(context).load("https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/10438582_10202555461926149_8285465463541146116_n.jpg?oh=a8b34b0ed406c9b2853da7ed9603e29c&oe=55FC98BA").into(contactViewHolder.vProfilePic);
 
         TinyDB userinfo =new TinyDB(context);
 
         profile_pic_url = userinfo.getString("profile_pic_url");
-
-
-
-
-
-
-
 
         Picasso.with(context)
                 .load(profile_pic_url)
                 .resize(225, 225)
                 .centerCrop()
                 .into(contactViewHolder.vProfilePic);
-        //contactViewHolder.vSurname.setText(ci.surname);
-        //contactViewHolder.vEmail.setText(ci.email);
-        //contactViewHolder.vTitle.setText("Rohit Tigga");
+
 
     }
 
@@ -77,30 +69,39 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ContactViewH
     public static class ContactViewHolder extends RecyclerView.ViewHolder {
 
         protected TextView vName;
-        protected TextView vSurname;
-        protected TextView vEmail;
         protected TextView vTitle;
         protected ImageView vProfilePic;
+        protected ImageButton vTextMessage;
 
         public ContactViewHolder(View v) {
             super(v);
             vName =  (TextView) v.findViewById(R.id.txtName);
-            //vSurname = (TextView)  v.findViewById(R.id.txtSurname);
-            //vEmail = (TextView)  v.findViewById(R.id.txtEmail);
+            vTextMessage = (ImageButton) v.findViewById(R.id.text_message);
             vTitle = (TextView) v.findViewById(R.id.title);
             vProfilePic = (ImageView) v.findViewById(R.id.profile_pic);
 
+
+
+            vTextMessage.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    //hopefully this works
+                    Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+                    smsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    smsIntent.setType("vnd.android-dir/mms-sms");
+                    smsIntent.putExtra("address", "4086079393");
+                    smsIntent.putExtra("sms_body","Body of Message");
+                    context.startActivity(smsIntent);
+
+                }
+            });
+
         }
+
+
+
+
+
     }
 
 
-    public void addItem( Event data) {
-        contactList.add(data);
-        notifyItemInserted(getItemCount()-1);
-    }
-
-    public void removeItem(int position) {
-        contactList.remove(position);
-        notifyItemRemoved(position);
-    }
 }
